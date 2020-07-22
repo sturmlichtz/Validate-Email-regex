c7053e8ca4480ace64f95d4238da4f631dc5dc22
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-22 17:26:59 +0530

    add validation of @ and 2nd Mandatory part

--- Validate_email_regex.sh
+++ Validate_email_regex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter email to validate :- " email
-email_Pattern="^[a-z]"
+email_Pattern="^[a-z][@]+[a-z]"
 if [[ $email =~ $email_Pattern ]]
 then
    echo "Valid email address"
